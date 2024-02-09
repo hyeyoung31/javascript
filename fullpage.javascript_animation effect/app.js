4d4e9d00fc6b61e993178db1d0527fe4d9ed8c8a
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-02-09 23:45:38 +0900

    first commit
    -Initialization using vanilla JavaScript
    -TimelineMax
    -TweenMax

---
+++ fullpage.javascript_animation effect/app.js
@@ -0,0 +1,14 @@
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+
+  }
+});
