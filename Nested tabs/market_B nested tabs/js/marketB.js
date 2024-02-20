f6cbd3643f1074cce044e2dafca3a4fc6c6ed925
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-02-20 23:20:41 +0900

    first commit
    have to find the reason of error that second, thrid tab contents don't show up

---
+++ Nested tabs/market_B nested tabs/js/marketB.js
@@ -0,0 +1,61 @@
+var nestedTabSelect = (tabsElement, currentElement) => {
+   const tabs = tabsElement; /*?? 'ul.tabs';*/
+
+   const currentClass = currentElement; /*?? 'active';*/
+   console.log(currentClass);
+
+   var test =  document.querySelectorAll(tabs)
+   console.log(test);
+   document.querySelectorAll(tabs).forEach(function (tabContainer) {
+     let activeLink, activeContent;
+     const links = Array.from(tabContainer.querySelectorAll("a"));
+     console.log(links);
+     // test
+     
+
+
+     activeLink = links.find(function (link) {
+         return link.getAttribute("href") === location.hash;
+       }) || links[0];
+       console.log(links[0]);
+       // links[0].classList.add(currentClass);
+     // 위에 아래중 하나는 적어줘야 처음 로딩했을 때 첫번째 탭에 ACTIVE;
+     console.log(activeLink);
+     activeLink.classList.add(currentClass);
+   
+ 
+     activeContent = document.querySelector(activeLink.getAttribute("href"));
+      console.log(activeContent);
+     activeContent.classList.add(currentClass);
+ 
+     links.forEach(function (link) {
+       if (link !== activeLink) {
+         const content = document.querySelector(link.getAttribute("href"));
+         // console.log(content);
+         content.classList.remove(currentClass);
+       }
+     });
+ 
+     tabContainer.addEventListener("click", function (e) {
+       if (e.target.tagName === "A") {
+
+         activeLink.classList.remove(currentClass);
+         activeContent.classList.remove(currentClass);
+ 
+
+         activeLink = e.target;
+         console.log(activeLink);
+         activeContent = document.querySelector(activeLink.getAttribute("href"));
+         console.log(activeContent);
+         activeLink.classList.add(currentClass);
+         activeContent.classList.add(currentClass);
+ 
+         e.preventDefault();
+       }
+     });
+   });
+ };
+ 
+ nestedTabSelect('ul.tabs', 'active');
+
+ 
